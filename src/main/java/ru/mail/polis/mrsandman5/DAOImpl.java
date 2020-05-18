9f910c808fb10c62a23c648e9139f8020166ec6f
MrSandman5 <mrsandman138@gmail.com>
2020-05-18 11:31:14 +0300

    Codeclimate fix

--- src/main/java/ru/mail/polis/mrsandman5/DAOImpl.java
+++ src/main/java/ru/mail/polis/mrsandman5/DAOImpl.java
@@ -20,9 +20,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.NavigableMap;
 import java.util.Objects;
-import java.util.Map;
 import java.util.TreeMap;
 import java.util.stream.Stream;
 
